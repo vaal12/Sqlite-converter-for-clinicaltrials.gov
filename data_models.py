c45fc89e7cfed4caab49344d9a80b608911d0970
Alexey Vassiliev <vaal12@yandex.ru>
2018-10-25 00:30:31 +0300

    1. Creating actual tables from the XML 2. List of db model fields and doc strings

--- data_models.py
+++ data_models.py
@@ -1,15 +1,60 @@
 from sqlalchemy import Column, String, Integer, Date
-
 from sqlalchemy_base import Base
 
 
-class Actor(Base):
-    __tablename__ = 'actors'
+class Study(Base):
+    __tablename__ = 'studies'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    birthday = Column(Date)
 
-    def __init__(self, name, birthday):
-        self.name = name
-        self.birthday = birthday
+    allocation	 = Column(String, 	doc = "study_design_info > allocation")
+    brief_summary 	 = Column(String, 	doc = "brief_summary > textblock")
+    brief_title 	 = Column(String, 	doc = "brief_title")
+    completion_date	 = Column(String, 	doc = "completion_date")
+    detailed_description 	 = Column(String, 	doc = "detailed_description > textblock")
+    has_expanded_access 	 = Column(String, 	doc = "has_expanded_access")
+    intervention_model 	 = Column(String, 	doc = "study_design_info > intervention_model")
+    last_known_status	 = Column(String, 	doc = "last_known_status")
+    last_update_submitted	 = Column(String, 	doc = "last_update_submitted")
+    masking 	 = Column(String, 	doc = "study_design_info > masking")
+    nct_id 	 = Column(String, 	doc = "id_info > nct_id")
+    official_title	 = Column(String, 	doc = "official_title")
+    org_study_id	 = Column(String, 	doc = "id_info > org_study_id")
+    overall_status 	 = Column(String, 	doc = "overall_status")
+    phase	 = Column(String, 	doc = "phase")
+    primary_purpose 	 = Column(String, 	doc = "study_design_info > primary_purpose")
+    source 	 = Column(String, 	doc = "source")
+    start_date	 = Column(String, 	doc = "start_date")
+    study_first_submitted	 = Column(String, 	doc = "study_first_submitted")
+    study_first_submitted_qc	 = Column(String, 	doc = "study_first_submitted_qc")
+    study_type 	 = Column(String, 	doc = "study_type")
+    url	 = Column(String, 	doc = "required_header > url")
+
+    def __init__(self):
+        pass
+#END class Study(Base):
+
+class LocationCountry(Base):
+    __tablename__ = 'location_countries'
+
+    id = Column(Integer, primary_key=True)
+    nct_id	 = Column(String, 	doc = "id_info > org_study_id")
+    country	 = Column(String, 	doc = "id_info > nct_id")
+
+    def __init__(self):
+        pass
+#END class LocationCountry(Base):
+
+
+
+
+# class Actor(Base):
+#     __tablename__ = 'actors'
+
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     birthday = Column(Date)
+
+#     def __init__(self, name, birthday):
+#         self.name = name
+#         self.birthday = birthday
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ print("Hello")
 PATH_TO_XML_FILES = r"c:\\Dev\\06. Data\\1. XML from FDA\\03.1 test XML data from FDA\\"
 
 from sqlalchemy_base import Base,  engine, Session
+import sqlalchemy.orm.attributes
 
-from data_models import Actor
+from data_models import Study, LocationCountry
 from datetime import date
+
 
 
 # 2 - generate database schema
@@ -14,9 +16,24 @@ Base.metadata.create_all(engine)
 # 3 - create a new session
 session = Session()
 
-matt_damon = Actor("Matt Damon", date(1970, 10, 8))
+# matt_damon = Actor("Matt Damon", date(1970, 10, 8))
 
-session.add(matt_damon)
+new_study = Study()
+
+studies_field_list = [col for col in dir(Study) if type(getattr(Study, col)) is (sqlalchemy.orm.attributes.InstrumentedAttribute)]
+print("Field list: \n{}".format(studies_field_list))
+
+studies_field_doc_strs = [getattr(getattr(Study, col), "doc") for col in studies_field_list ]
+
+print studies_field_doc_strs
+
+for field in studies_field_list:
+    if field <> "id":
+        setattr(new_study, field, "I am a field:"+field)
+
+session.add(new_study)
+
+# session.add(matt_damon)
 
 # 10 - commit and close session
 session.commit()
