40ad0bb41a63a4cafe6c39b06e7a76715e467fdc
Alexey Vassiliev <vaal12@yandex.ru>
2018-11-01 20:33:47 +0200

    Added check for NCT id in DB. Skipping if present.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+#To start this - run pipenv shell
+# and then runner bat - it will compost old test logs etc.
+
+
 import logging
 
 
@@ -6,10 +11,10 @@ import logging
 
 
 #TEST DATA:
-# XML_FILE_DIR = "c:\\Dev\\06. Data\\1. XML from FDA\\03.1 test XML data from FDA\\" 
+XML_FILE_DIR = "c:\\Dev\\06. Data\\1. XML from FDA\\03.1 test XML data from FDA\\" 
 
 #ACTUAL DATA
-XML_FILE_DIR = "c:\\Dev\\06. Data\\1. XML from FDA\\04. NCT FULL set 20Oct\\" 
+# XML_FILE_DIR = "c:\\Dev\\06. Data\\1. XML from FDA\\04. NCT FULL set 20Oct\\" 
 
 
 
@@ -24,16 +29,17 @@ import os
 from xml_parsing_routines import parserXMLNCTFile
 
 
-def parseXMLFilesInDir(fileDirPath, totalFileNumberInDir, tables_writers_list, session):
+def parseXMLFilesInDir(fileDirPath, totalFileNumberInDir, tables_writers_list, session, db_engine):
         fileCounter = 0
         objects_created = []
         for dirName, subdirList, fileList in os.walk(fileDirPath):
                 print('Found directory: %s' % dirName)
                 for fname in fileList:
+
                         if fname[-3:].lower() <> "xml":
                                 logging.debug("Non XML file. {}  SKIPPING".format(fname))
                                 continue
-                        # print(colorama.ansi.clear_screen())
+
                         fileCounter +=1
                         print("DIR:"+dirName)
                         print("  FILE:"+fname)
@@ -45,6 +51,17 @@ def parseXMLFilesInDir(fileDirPath, totalFileNumberInDir, tables_writers_list, s
                                                 (fileCounter*100/totalFileNumberInDir)))
                         # logging.debug("Current file number:%i"%(fileCounter))
                         # print("\t\t Total number of files:%i"%(totalFileNumberInDir))
+
+                        #CHECK IN DB if this NCT was parsed
+                        
+                        file_nct_name = fname[:-4]
+                        logging.debug("file name for NCT check :{}".format(file_nct_name))
+                        rows = db_engine.execute("SELECT COUNT(*) FROM studies WHERE nct_id = '{}'".format(file_nct_name)).fetchall()
+                        nct_id_count = rows[0][0]
+                        logging.debug("Result:{}".format(nct_id_count))
+
+                        if nct_id_count > 0: continue
+                        
                         objs_created = parserXMLNCTFile(dirName+"\\\\"+fname,
                                                 tables_writers_list)
                         print("Objs created:{}".format(objs_created))
@@ -80,7 +97,7 @@ if __name__ == "__main__":
         # 3 - create a new session
         session = Session()
 
-        parseXMLFilesInDir(XML_FILE_DIR, 1000, models_list, session)
+        parseXMLFilesInDir(XML_FILE_DIR, 1000, models_list, session, engine)
 
         # 10 - commit and close session
         session.commit()
